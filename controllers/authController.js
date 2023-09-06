d5bb5c91c5aa759fa0d9263d6d38581cb9cfe8b6
Vito Russo <withteeth.vito@gmail.com>
2023-09-06 19:24:03 +0200

    removed domain option

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,6 @@ const createSendToken = (user, statusCode, res) => {
 		expires: new Date(
 			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
 		),
-		domain: '.onrender.com',
 		secure: true,
 		httpOnly: true,
 		sameSite: 'none',
