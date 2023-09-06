0348e3e52c0a101327e5d5b21220f9d8d573dfeb
Vito Russo <withteeth.vito@gmail.com>
2023-09-06 18:22:28 +0200

    minor changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ const createSendToken = (user, statusCode, res) => {
 		expires: new Date(
 			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
 		),
-		//secure: true,
+		secure: true,
 		httpOnly: true,
 	};
 	user.password = undefined;
